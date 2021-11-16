149068815d720eca6aecaa6a54642cd14b67be39
Brandon Ortiz <brandonhalonyc@gmail.com>
2021-11-16 02:54:15 -0500

    Update Port_Scanner.py

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -8,7 +8,7 @@ results = sock.connect_ex((target,port))
 #Initiate TCP Handshake
 if sock.connect_ex((target,port)):
   print(results)
-  #If Connection is Successful
+  #If Connection is Successful (socket.connect_ex returns the value of 0 if the TCP connection is recieved)
 if results == 0:
     print("The port is open! ")
     print("The IP address of the website is: " + IP)
